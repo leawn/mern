638c92ff952574ee7de5b170f44e0e82ac319f13
Leon <leonrubner2809@gmail.com>
2021-01-09 16:47:27 +0100

    added navigation

--- frontend/src/shared/components/Navigation/MainNavigation.js
+++ frontend/src/shared/components/Navigation/MainNavigation.js
@@ -1,23 +1,36 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 
 import './MainNavigation.css';
 import MainHeader from "./MainHeader";
+import NavLinks from "./NavLinks";
+import SideDrawer from "./SideDrawer";
 
 const MainNavigation = () => {
-    return <MainHeader>
-        <button className='main-navigation__menu-btn'>
-            <span />
-            <span />
-            <span />
-        </button>
-        <h1 className='main-navigation__title'>
-            <Link to='/'>YourPlaces</Link>
-        </h1>
-        <nav>
-            ...
-        </nav>
-    </MainHeader>
+    const [drawerIsOpen, setDrawerIsOpen] = useState(false);
+
+    return (
+        <>
+            <SideDrawer>
+                <nav className='main-navigation__drawer-nav'>
+                    <NavLinks />
+                </nav>
+            </SideDrawer>
+            <MainHeader>
+                <button className='main-navigation__menu-btn'>
+                    <span />
+                    <span />
+                    <span />
+                </button>
+                <h1 className='main-navigation__title'>
+                    <Link to='/'>YourPlaces</Link>
+                </h1>
+                <nav className='main-navigation__header-nav'>
+                    <NavLinks />
+                </nav>
+            </MainHeader>
+        </>
+    )
 }
 
 export default MainNavigation;
