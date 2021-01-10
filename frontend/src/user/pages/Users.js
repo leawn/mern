743cf696c36a90d35352e2b8491e5e3b8116acf2
Leon <leonrubner2809@gmail.com>
2021-01-10 12:23:32 +0100

    frontend

--- frontend/src/user/pages/Users.js
+++ frontend/src/user/pages/Users.js
@@ -1,19 +1,19 @@
 import React from 'react';
-import UsersList from "../components/UsersList";
+
+import UsersList from '../components/UsersList';
 
 const Users = () => {
-    const USERS = [
-        {
-            _id: 'u1',
-            name: 'Leo',
-            image: 'https://i.ytimg.com/vi/M2u20FQiYok/maxresdefault.jpg',
-            places: 2
-        }
-    ];
+  const USERS = [
+    {
+      id: 'u1',
+      name: 'Max Schwarz',
+      image:
+        'https://images.pexels.com/photos/839011/pexels-photo-839011.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
+      places: 3
+    }
+  ];
 
-    return (
-        <UsersList items={USERS}/>
-        );
-}
+  return <UsersList items={USERS} />;
+};
 
 export default Users;
